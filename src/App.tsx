46306a994422584c85af30e74fbc087e1e969b8b
Matheus Magalhaes <magal97@outlook.com>
2020-06-18 13:35:16 -0300

    :tada: Adding AOS Lib

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+import AppProvider from './hooks'
+
+function App() {
+  return (
+    <>
+      <AppProvider>
+      <Routes/>
+      </AppProvider>
+      <GlobalStyle/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MainText/index.tsx
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from 'react';
+import {useTransition, animated} from 'react-spring';
+import './styles';
+
+const MainText = () => {
+    const [toggle, set] = useState(false)
+    const transitions = useTransition(toggle, null, {
+    from: { display: 'flex', opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    })
+    
+    return (
+        <span>
+            {transitions.map(({ item, key, props }) => (
+            item
+            ? <animated.div style={props}>Matheus</animated.div>
+            : <animated.div style={props}>Developer</animated.div>
+            ))}
+        </span>
+    );
+    
+    
+    
+    
+};
+    
+export default MainText;
